cc00396a2afee6e98d34b6413071d2a69c50490d
4eeroke <renal.usm@bk.ru>
2023-04-14 01:23:51 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import Ikon from "../../assets/img/shopping-cart.png";
+import logo from "../../assets/img/logo.svg";
+import style from "./Header.module.css";
+
+const Header = ({ setActive }) => {
+  return (
+    <header>
+      <NavLink to={"/"}>
+        <div className={style.logo}>
+          <img src={logo} alt="" />
+        </div>
+      </NavLink>
+      <nav className={style.nav}>
+        <NavLink className={style.navlink} to={"/"}>
+          Главная
+        </NavLink>
+        <NavLink className={style.navlink} to={"#catalog"}>
+          Каталог
+        </NavLink>
+      </nav>
+      <div className={style.btns_header}>
+        <div className={style.ikon} onClick={() => setActive(true)}>
+          <img src={Ikon} alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
